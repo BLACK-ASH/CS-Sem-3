dbd9d489d00bca3366feadaf24a358022a585086
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2025-07-28 18:59:06 +0530

    DS Practical Updated

--- DataStructure/code/polynomial.py
+++ DataStructure/code/polynomial.py
@@ -23,41 +23,52 @@ class Polynomial:
         print(equation)
                 
     def insert(self,c,v,e):
-        curr = self.head
-        if curr == None:
-            newNode = Node(c,v,e,curr)
+        # If Coeficient Is Zero
+        if c == 0:
+            return True
+
+        newNode = Node(c,v,e)
+
+        # Case 1 : Linked List Is Empty
+        if self.head is None:
             self.head = newNode
             return True
 
-        # If Coeffient Is Greater Than Head
-        if e >= curr.e:
-            # If Variable With Same Power Exist
-            if curr.v == v and curr.e==e:
-                curr.c += c
-                return True
-                
-            newNode = Node(c,v,e,curr)
+        # Case 2 : Insert At The Beginning
+        if e > self.head.e:
+            newNode.nextNode = self.head
             self.head = newNode
             return True
-            
-        # If Coeffient Is Smaller Than Head
+        # If Exponential Already Exist
+        elif e == self.head.e and v == self.head.v:
+            self.head.c += c
+            if self.head.c == 0:
+                self.head = self.head.nextNode
+            return True
 
-        # 1. Finding Position
-        while curr.nextNode:
+        # Case 3 : Insert At The Middle Or In The End
+        prev = self.head
+        curr = self.head.nextNode
+
+        while curr:
+            # Finding Position
             if e > curr.e:
                 break
+            # If Exponential Already Exist
+            elif e == curr.e and v == curr.v:
+                curr.c += c
+                if curr.c == 0:
+                    curr = curr.nextNode
+                return True
+            # Shifting Pointer
+            prev = curr
             curr = curr.nextNode
 
-        # 2. Adding Node
-        # If Variable With Same Power Exist
-        if curr.v == v and curr.e==e:
-            curr.c += c
-            return True
-        
-        newNode = Node(c,v,e,curr.nextNode)
-        curr.nextNode = newNode
+        # Insert Node
+        newNode.nextNode = curr
+        prev.nextNode = newNode
         return True
-
+            
     def add(self,e):
         curr = e.head
         while curr:
@@ -76,21 +87,25 @@ class Polynomial:
 # First Equation
 p = Polynomial()
 p.insert(2,"x",3)
-p.insert(2,"x",4)
 p.insert(2,"x",5)
-p.print()
+p.insert(2,"x",4)
+print("First Equation : ",end="");p.print()
 
 # Second Equation
 e = Polynomial()
 e.insert(2,"x",1)
-e.insert(2,"x",1)
-e.insert(3,"x",5)
-e.print()
+e.insert(2,"x",2)
+e.insert(-3,"x",5)
+print("Second Equation : ",end="");e.print()
 
 # Adding Equation
+print("")
+print("Before Adding : ",end="");p.print()
 p.add(e)
-p.print()
+print("After Adding : ",end="");p.print()
 
 # Subtracting Equation
+print("")
+print("Before Subtracting : ",end="");p.print()
 p.subtract(e)
-p.print()
+print("After Subtracting : ",end="");p.print()
